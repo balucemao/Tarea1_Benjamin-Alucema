636899cb17b42f1b10e1c66f561265eca5ff9143
balucemao <benjamin.alucema.o@mail.pucv.cl>
2026-04-13 00:37:31 +0000

    FINAL

--- README.md
+++ README.md
@@ -1,33 +1,57 @@
-En este *repl* puedes encontrar varios ejemplos que te pueden ayudar con las tareas de estructura.
+# SMART_TODO - PLANIFICADOR DINAMICO DE TAREAS
 
-## Código de Ejemplo (tarea1)
-Para ejecutar el ejemplo tarea1 primero debemos compilar (en la carpeta raíz)
-````
-gcc tdas/*.c tarea1.c -Wno-unused-result -o tarea1
-````
+Este proyecto consiste en una aplicacion/programa en el lenguaje C.
+Consiste en llevar un registro de tareas ordenadas en orden estricto de llegada, para lograr esto se utiliza el TDA cola cuya principal caracteristica es mantener su principio FIFO, el primero en entrar a la cola es el primero en salir.
 
-Y luego ejecutar:
-````
-./tarea1
-````
+Otro punto a tener en consideracion del modo de funcionamiento es que estas tareas tienen etiquetas denominadas CATEGORIAS para identificarlas, por cada tarea ademas, se debe asignar la hora de ingreso al sistema de esta tarea y tambien el objetivo/accion que se requiere almacenar como tarea.
 
-## TDAs
-En la carpeta `tdas` se encuentran implementados distintos TDAs que puedes utilizar (lista, pila, cola, cola con prioridad y mapas). 
+# Para poder compilar este programa se deben tener instalados los archivos TDAs dentro de la carpeta (tdas), utilizados para poder hacer funcionar este codigo (en el programa que estas usando para compilar). 
+- Los archivos correspondientes son: extra.c, list.c, y queue.h.
+- Archivos para entender que hace cada funcion: extra.h y list.h. (En estos archivos viene explicado como funciona la logica de los TDAs usados en mi programa)
+- Archivo tarea1.c donde esta el codigo para que funcione la aplicacion
 
-Las implementaciones no son las más eficientes (todas usan como estructura de datos una **lista enlazada**), por lo que puedes reemplazarlas por las que has realizado en los labs.
+# Luego de tener ya instalado estos archivos necesitas ejecutar el siguiente comando en la terminal:
 
-## Otros códigos (en carpeta examples)
-Para ejecutar los distintos ejemplos que hay en la carpeta `examples`, primero debes compilarlos. Si estamos en la carpeta raíz:
-````
-gcc tdas/*.c examples/example2_menu.c -Wno-unused-result -o example
-````
-Y luego ejecutarlos:
-````
-./example
-````
+```bash
+gcc tarea1.c tdas/*.c -o smart_todo
+```
 
-Se incluyen los siguientes ejemplos:
-* `example1_list`: Uso del TDA Lista, inserción y eliminación de elementos.
-* `example2_menu`: Ejemplo de menú con submenús.
-* `example3_readcsv`: Ejemplo de lectura desde un archivo csv y almacenamiento en datos estructurados.
-* `example4_map`: Ejemplo de uso del TDA mapa.
+# Finalmente debes ejecutar la aplicacion utilizando este codigo en la terminal:
+
+```bash
+./smart_todo
+```
+
+# FUNCIONES PRINCIPALES:
+
+- registrar_categoria: Esta funcion permite al usuario registrar una categoria, si existe se lo informa al usuario, de lo contrario inserta la categoria en la lista de categorias. Idealmente la categoria sin tildes o ñ. Maximo 50 caracteres.
+
+- eliminar_categoria: Esta funcion funciona como un "vaciado" o borron, pide al usuario una categoria y elimina sus derivados, primero la categoria y todas sus tareas ramificadas, luego muestra si se borro la categoria, y tambien tareas (si es que habian tareas en cola que formaban parte de esa categoria).
+
+- mostrar_categorias: Muestra todas las categorias actuales, permite al usuario decidir si es posible encolar tareas en una categoria existente, o primero crear la categoria. Si no existen categoria pide registrar una antes.
+
+- registrar_pendiente: Permite al usuario (si es que hay categorias) poner en la cola la tarea que desea el usuario, pide primero la categoria a la que pertenece, si existe luego pide ingresar la descripcion de la tarea, finalmente se ingresa en la cola la tarea con la hora actual, nombre de categoria y el objetivo de la tarea. Si no existen categorias pide ingresar una antes. Tarea maximo 100 caracteres por objetivo.
+  
+- atender_siguiente: Primero verifica se existen tareas pendientes, sino pide ingresar una antes. Si efectivamente existen tareas en cola entonces primero la muestra junto con todos sus datos: hora, objetivo y categoria de pertenencia. Luego la elimina y deja al inicio de la cola la siguiente tarea ingresada al sistema.
+  
+- mostrar_tablero: Muestra todas las tareas pendientes (mas antigua a mas reciente), su categoria, objetivo y fecha. Si no existen tareas informa al usuario.
+  
+- filtrar_por_categoria: Permite al usuario conocer todas las tareas en cola que pertenecen a una determinada categoria. Si no existen categorias lo informa, tambien informa si existe la categoria pero no tiene tareas en cola.
+
+## FUNCION(ES) SECUNDARIA
+
+- aMayus: convierte una cadena (palabra) en mayuscula, esto es util al ingresar las categorias, ya que quiza el usuario olvida como escribio la categoria, o tuvo un error, al convertir todo a mayuscula disminuye mucho el riesgo de un error de ese estilo. Por ejemplo hola = HOLA = HoLa = HOla
+
+# EJEMPLO DE FUNCIONAMIENTO DEL PROGRAMA
+
+  - Usuario ingresa opcion 1, primera categoria HOGAR.
+  - Usuario ingresa opcion 1 (nuevamente), segunda categoria COCINA.
+  - Usuario ingresa opcion 4, ingresa la tarea LAVAR PLATOS en la categoria HOGAR.
+  - Usuario ingresa opcion 4 (nuevamente), ingresa la tarea COCINAR CENA en la categoria COCINA.
+  - Usuario ingresa opcion 6, se visualiza el tablero general con la tarea LAVAR PLATOS y luego COCINAR CENA, ambas con hora exacta.
+  - Usuario ingresa opcion 5, se muestra la tarea en cabeza LAVAR PLATOS, esta se elimina y queda en cabeza de la cola COCINAR CENA.
+  - Usuario ingresa opcion 7, ingresa la categoria HOGAR, se informara que ya no existen tareas en esa categoria.
+  - Usuario ingresa opcion 3, se mostrara la categoria HOGAR Y COCINA.
+  - Usuario ingresa opcion 2, ingresa la categoria HOGAR, se elimina del sistema.
+  - Usuario ingresa opcion 3, se mostrar la categoria COCINA.
+  - Usuario ingresa opcion 8, se sale del programa.
--- tarea1.c
+++ tarea1.c
@@ -23,7 +23,7 @@ typedef struct{
 void mostrarMenuPrincipal() {
   limpiarPantalla();
   puts("========================================");
-  puts("     Sistema de Gestión Hospitalaria");
+  puts("     Sistema de Gestión SMART_TODO");
   puts("========================================");
 
   puts("1) Nueva Categoría");
@@ -36,176 +36,283 @@ void mostrarMenuPrincipal() {
   puts("8) Salir");
 }
 
+//funcion auxiliar, convierte cadena (plabra) a mayusculas
 void aMayus(char *categoria){
   for (int i = 0; categoria[i] != '\0'; i++){
     categoria[i] = toupper((unsigned char)categoria[i]);
   }
 }
 
+//funciones requeridas
+
+// 1- REGISTRAR NUEVA CATEGORIA
 void registrar_categorias(List *categorias) {
   printf("Registrar nueva categoría (sin tildes o ñ)\n");
-  //nomre categoria nueva
-  char nombreNuevo[50];
-  scanf(" %s", nombreNuevo);
-
-  //convertir a mayusculas para asegurar que no se repita categoria
-  aMayus(nombreNuevo);
   
-  //variable auxiliar tipo CATEGORIAS para revisar si la categoria no se repite
+  //nomre categoria nueva
+  char nuevaCategoria[50];
+  scanf(" %49[^\n]", nuevaCategoria);
+
+  //convertir a mayusculas para asegurar que no se repita categoria.
+  aMayus(nuevaCategoria);
+  
+  //variable auxiliar tipo CATEGORIAS, recibe la primera categoria en lista.
   CATEGORIAS *revisarDuplicados = list_first(categorias);
+  //se compara la nueva categoria con cada categoria en lista, si no existe se agrega.
   while(revisarDuplicados != NULL){
-    //se revisa la lista completa de categorias para ver si se repite alguna
-    if (strcmp(revisarDuplicados->nombre, nombreNuevo) == 0){
+    //si se repite entonces se descarta.
+    if (strcmp(revisarDuplicados->nombre, nuevaCategoria) == 0){
       printf("Ya existe esa categoria\n");
       return;
     }
     revisarDuplicados = list_next(categorias);
   }
+
+  //se crea una nueva categoria.
   CATEGORIAS *nueva = (CATEGORIAS*)malloc(sizeof(CATEGORIAS));
-  //se copia el nuevo nombre en la lista
-  strcpy(nueva->nombre, nombreNuevo);
+  //se copia el nuevo nombre en la categoria creada recien.
+  strcpy(nueva->nombre, nuevaCategoria);
+  //se inserta la nueva categoria en la lista.
   list_pushBack(categorias, nueva);
   printf("Categoria registrada correctamente\n");
 }
 
-void mostrar_categorias(List *categorias) {
-  // Mostrar categorías
-  printf("Categorías:\n");
-  if(categorias == NULL)return;
-  CATEGORIAS *actual = list_first(categorias);
-  //si no hay categorias en la lista
-  if (actual == NULL){
-    printf("Debe registrar una categoria antes (opcion 1)\n");
-    return;
-  }
-  //mientras la lista no este vacia se muestra las categorias
-  while(actual != NULL){
-    printf("%s\n", actual->nombre);
-    actual = list_next(categorias);
-  }
-}
 
+// 2- eliminar categoria
 void eliminar_categoria(List *categorias, Queue *cola){
-  char aEliminar[50];
-  //se revisa si la lista esta vacia o es nula
+  
+  //se revisa si existe categorias en la lista
   if (list_first(categorias) == NULL || categorias == NULL){
-    printf("No existen categorias, porfavor ingrese una (opcion 1), o elija otra opcion\n");
+    printf("No existen categorias, porfavor elija otra opcion\n");
     return;
   }
-  
-  printf("Tarea a eliminar\n");
-  scanf(" %s", aEliminar);
 
-  //convertir a mayusculas para asegurar que no se repita categoria
+  //aqui estara el nombre de la categoria a eliminar
+  char aEliminar[50];
+  printf("Categoria a eliminar\n");
+  //el usuario ingresa la categoria a eliminar
+  scanf(" %49[^\n]", aEliminar);
+
+  //convertir a mayusculas para asegurar que no se repita categoria.
   aMayus(aEliminar);
-  
-  CATEGORIAS *catAeliminar = list_first(categorias);
 
-  //si se encuentra la categoria a eliminar
+  //aqui se almacena la primera categoria de la lista.
+  CATEGORIAS *catActual = list_first(categorias);
+
+  //aqui se verifica si existe la categoria.
   int encontrado = 0;
 
-  //buscar categoria a eliminar en la lista de categorias
-  while (catAeliminar != NULL){
-    if (strcmp(catAeliminar->nombre, aEliminar) == 0){
-      list_popCurrent(categorias);
-      encontrado = 1;
+  //buscar categoria a eliminar en la lista de categorias.
+  while (catActual != NULL){
+    //se cmpara la categoria a eliminar con todas las categorias en lista.
+    if (strcmp(catActual->nombre, aEliminar) == 0){
+      CATEGORIAS *categoriaAux = list_popCurrent(categorias); //aqui accedo por completo a la lista de categoria, categoria actual
+      free(categoriaAux); //se elimina la categoria actual de mi lista de categorias
+      encontrado = 1; //aqui verifico que se encontro la categoria
       break;
     }
-    catAeliminar = list_next(categorias);
+    //con esto recorro la lista hasta encontrar categoria buscada
+    catActual = list_next(categorias);
   }
 
-  //si es que no existe la categoria
+  //si es que no existe la categoria en la lista de categorias
   if (encontrado == 0){
-    printf("No existe esa categoria, ingrese una nueva (opcion 1) o elige otra opcion\n");
+    printf("No existe esa categoria, otra opcion\n");
     return;
   }
 
   //se crea cola para pasar las tareas que no pertenecen a la categoria buscada
   Queue *colaAux = queue_create(NULL);
   //el ciclo busca todas las tareas coincidentes con la categoria, si coniciden la borra de la cola, sino la insertta al final de la cola auxiliar
+
+  int borradas = 0; //verifica si se borraron tareas para esa categoria
+  
   while(queue_front(cola) != NULL){
-    TAREAS *t = queue_remove(cola);
-    if (strcmp(t->categoria, aEliminar) == 0)free(t);
-    else queue_insert(colaAux, t);
+    TAREAS *tareaActual = queue_remove(cola); //aqui obtengo la primera tarea de la cola y esta se elimina de la cola
+    
+    if (strcmp(tareaActual->categoria, aEliminar) == 0){
+      free(tareaActual); //si categoria de tarea actual coincide con categoria a eliminar, se elimina la tarea
+      borradas += 1;
+    }
+    else queue_insert(colaAux, tareaActual); //de lo contrario la tarea se inserta en la cola auxiliar 
+
   }
   //ciclo para devolver a la cola original en orden original
   while(queue_front(colaAux) != NULL){
+    //queue_remove elimina elemento de cola y devuelve el primero de la cola
     queue_insert(cola, queue_remove(colaAux));
   }
-  free(catAeliminar);
+  
+  //se libera la memoria de la cola auxiliar
   free(colaAux);
-  printf("Lista y cola elimininada\n");
+  if (borradas > 0) printf("Categoria %s y sus tareas eliminadas.\n", aEliminar, borradas);
+  else printf("Categoria eliminada, no tenia tareas asociadas.\n");
 }
 
+// 3- mostrar categorias
+void mostrar_categorias(List *categorias) {
+  // Mostrar categorías
+  printf("Categorías:\n");
+  
+  if(categorias == NULL)return; //aqui se comprueba si es que existen categorias en la lista
+  
+  CATEGORIAS *actual = list_first(categorias); //aqui se obtiene la primera categoria
+  //si no hay categorias en la lista
+  if (actual == NULL){
+    printf("Debe registrar una categoria antes\n");
+    return;
+  }
+  //mientras la lista no este vacia se muestra las categorias
+  while(actual != NULL){
+    printf("%s\n", actual->nombre);
+    actual = list_next(categorias); //aqui se recorre la lista de categorias
+  }
+}
 
+// 4-registrar tarea pendiente
 void registrar_pendiente(List *categorias, Queue *cola){
-  char nombreCategoria[50];
-  printf("A que categoria pertenece la tarea?\n");
-  scanf(" %s", nombreCategoria);
-
-  //convertir a mayusculas para asegurar que no se repitan categorias
-  aMayus(nombreCategoria);
   
   //se revisa si la lista esta vacia
   if (list_first(categorias) == NULL || categorias == NULL){
-    printf("No hay categorias, ingresar (opcion 1)\n");
+    printf("No hay categorias, ingresar otra opcion\n");
     return;
   }
+
+  char nombreCategoria[50]; //aqui se almacena el nombre de la categoria 
+  printf("A que categoria pertenece la tarea?\n");
+  scanf(" %49[^\n]", nombreCategoria);
+  //convertir a mayusculas para asegurar que no se repitan categorias
+  aMayus(nombreCategoria);
   
-  CATEGORIAS *categoriaActual = list_first(categorias);
+  CATEGORIAS *categoriaActual = list_first(categorias); //aqui se guarda la primera categoria de la lista de categorias
 
-  int POSNULA = 0;
+  int POSNULA = 0; //variable que verifica si existe categoria
 
-  //si existe en la lista de categorias la categoria ingresada por el usuario, se inserta una tarea en esa categoria.
+  //aqui se recorrer la lista de categorias para saber si alguna coincide con la ingresada
   while(categoriaActual != NULL){
     if (strcmp(categoriaActual->nombre, nombreCategoria) == 0){
       POSNULA = 1;
       break;
     }
-    categoriaActual = list_next(categorias);
+    categoriaActual = list_next(categorias); //se recorre la lista de categorias
   }
   
   //si no se encuentra entonces pide elegir otra opcion.
   if (POSNULA == 0){
-    printf("La categoria no existe, primero crear (opcion 1)\n");
+    printf("La categoria no existe, ingresar otra opcion.\n");
     return;
   }
 
   //si existia la categoria entonces se crea variable de tipo tareas.
   TAREAS *nuevaTarea = (TAREAS*)malloc(sizeof(TAREAS));
-
+  
+  //se verifica si no hubo problemas al reservar memoria
   if (nuevaTarea == NULL)return;
 
   //se le inserta la categoria seleccionada en la variable tipo tareas, despues se pide una tarea/objetivo que se requiere y se inserta en el objetivo de la variable tareas
   strcpy(nuevaTarea->categoria, nombreCategoria);
   printf("Cual es el objetivo de tu tarea?\n");
+  //leer hasta el salto de linea
   scanf(" %[^\n]", nuevaTarea->objetivo);
 
   //se pide la hora actual y se inserta en la nueva tarea
   time_t hora = time(NULL);
+  hora -= 14400; //esto lo hice para que coincida con la hora en chile
   struct tm *tm = localtime(&hora);
-  strftime(nuevaTarea->fecha, 20, "%H:%M", tm);
+  strftime(nuevaTarea->fecha, 20, "%H:%M:%S", tm);
   //se inserta la nueva tarea en la cola de tareas
   queue_insert(cola, nuevaTarea);
+  printf("Tarea registrada a las %s.\n", nuevaTarea->fecha);
 }
 
-
+// 5- atender siguiente tarea, eliminarla y dejar la siguiente en cola
 void atender_siguiente(Queue *cola){
   TAREAS *tareaActual = queue_remove(cola);
   if (tareaActual == NULL){
-    printf("No hay pendientes\n");
+    printf("No hay pendientes.\n");
     return;
   }
-  printf("objetivo: %s\n", tareaActual->objetivo);
-  printf("categoria: %s | hora: %s\n", tareaActual->categoria, tareaActual->fecha);
+  printf("Objetivo: %s.\n", tareaActual->objetivo);
+  printf("Categoria: %s | hora: %s.\n", tareaActual->categoria, tareaActual->fecha);
   free(tareaActual);
   
+}
+
+// 6- mostrar tareas pendientes
+void mostrar_tablero(Queue *cola){
+  //se revisa si existen tareas pendiente.
+  if (queue_front(cola) == NULL){
+    printf("No existen tareas pendientes en el sistema\n");
+    return;
+  }
+
+  //cola auxiliar
+  Queue *colaAux = queue_create(NULL);
+  printf("TABLERO GENERAL (tareas en orden de llegada)\n");
+
+  //recorrer cola para enviar tareas a la auxiliar, mientras existan tareas
+  while(queue_front(cola) != NULL){
+    TAREAS *tareaActual = queue_remove(cola);
+    //mostrar tarea actual
+    printf("| %s | %s | registrada a las %s |\n", tareaActual->categoria, tareaActual->objetivo, tareaActual->fecha);
+    //insertar en cola axuliar la tarea actual
+    queue_insert(colaAux, tareaActual);
+  }
+
+  //bucle para obtener orden original
+  while(queue_front(colaAux) != NULL){
+    queue_insert(cola, queue_remove(colaAux));
+  }
+  //liberar la memoria de la cola auxiliar
+  free(colaAux);
+}
+
+// 7- filtrar tareas por una categoria
+void filtrar_por_categoria(List *categorias, Queue *colaTareas){
+  //se revisa si existen categorias en la lista de categorias
+  if (list_first(categorias) == NULL || categorias == NULL){
+    printf("No hay categorias, ingresar otra opcion\n");
+    return;
+  }
+
+  //categoria para filtrar
+  char catFiltrar[50];
+  printf("Ingrese la categoria para filtrar sus tarea derivadas\n");
+  scanf(" %49[^\n]", catFiltrar);
+  aMayus(catFiltrar); //categoria para filtrar a mayusculas
+
+  Queue *colaAux = queue_create(NULL); //se crea cola auxiliar
+  int POSNULA = 0;
+  printf("Tareas en la categoria %s:\n\n", catFiltrar);
+
+  //mostrar tareas de esa categoria
+  while(queue_front(colaTareas) != NULL){
+    //se crea variable tipo TAREAS que resive el primer elemento de la cola
+    //ese elemento tambien se elimina de la cola de tareas
+    TAREAS *tareaActual = queue_remove(colaTareas);
+    if (strcmp(tareaActual->categoria, catFiltrar) == 0){
+      printf("- %s (Registrada: %s)\n", tareaActual->objetivo, tareaActual->fecha);
+      POSNULA = 1;
+    }
+    //insertar tarea en la cola de tareas auxiliar
+    queue_insert(colaAux, tareaActual);
+  }
+
+  //se verifica si existen tareas en la categoria
+  if(POSNULA == 0)printf("No existen tareas de esa categoria.\n");
+
+  //devolver cola original a su orden corrector FIFO
+  while(queue_front(colaAux) != NULL){
+    queue_insert(colaTareas, queue_remove(colaAux));
+  }
+  //se libera la memoria de la cola auxiliar
+  free(colaAux);
 }
 
 int main() {
   char opcion;
   List *categorias = list_create(); // Lista para almacenar categorías
-  Queue *colaTareas = queue_create(NULL);
+  Queue *colaTareas = queue_create(NULL); //cola para almacenar tareas
   do {
     mostrarMenuPrincipal();
     printf("Ingrese su opción: ");
@@ -214,13 +321,15 @@ int main() {
 
     switch (opcion) {
     case '1':
+      //logica para registrar categorias
       registrar_categorias(categorias);
       break;
     case '2':
-      eliminar_categoria(categorias, colaTareas);
       // Lógica para eliminar una categoría
+      eliminar_categoria(categorias, colaTareas);
       break;
     case '3':
+      //logica para mostrar categorias
       mostrar_categorias(categorias);
       break;
     case '4':
@@ -228,13 +337,15 @@ int main() {
       registrar_pendiente(categorias, colaTareas);
       break;
     case '5':
-      atender_siguiente(colaTareas);
       // Lógica para atender al siguiente paciente
+      atender_siguiente(colaTareas);
       break;
     case '6':
+      mostrar_tablero(colaTareas);
       // Lógica para mostrar el tablero general
       break;
     case '7':
+      filtrar_por_categoria(categorias, colaTareas);
       // Lógica para filtrar por categoría
       break;
     case '8':
@@ -247,8 +358,18 @@ int main() {
 
   } while (opcion != '8');
 
-  // Liberar recursos, si es necesario
-  list_clean(categorias);
+  //bucle para eliminar todas las categorias individualmente
+  while(list_size(categorias) > 0){
+    free(list_popFront(categorias));
+  }
+  free(categorias); //eliminar lista de categorias
 
+  //bucle para eliminar todas las tareas individuales en la cola
+  while(queue_front(colaTareas) != NULL){
+    free(queue_remove(colaTareas));
+  }
+  free(colaTareas); //eliminar cola
+  
+  
   return 0;
 }
